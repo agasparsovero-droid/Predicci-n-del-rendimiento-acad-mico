45a25b4d849677c6c2f0d36da6a07d7409981490
agasparsovero-droid <agasparsovero@gmail.com>
2025-10-12 22:47:05 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,3 @@
+promedio = st.number_input("Promedio de notas", min_value=0.0, max_value=20.0)
+motivacion = st.slider("Motivación", 1, 5, 3)
+responsabilidad = st.slider("Responsabilidad", 1, 4, 2)
